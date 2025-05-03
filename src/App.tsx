176aa2ae526e43f5225b32bdef31eeb109d657e0
jayeann <jgtagoctoc@gmail.com>
2025-05-03 17:56:20 +0800

    add basic home ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import triviaLogo from "/trivia.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
+      <div className="flex items-center justify-center">
         <a href="#" target="_blank">
-          <img src={triviaLogo} className="logo" alt="Vite logo" />
+          <img className="size-96" src={triviaLogo} alt="Vite logo" />
         </a>
       </div>
-      <h1>Name that</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
+      <h1 className="uppercase text-9xl text-amber-300 font-black">
+        Name that
+      </h1>
+
+      <button className="px-8 py-4 bg-gradient-to-br from-green-400 to-green-700 text-white text-2xl font-extrabold rounded-full shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-xl active:scale-95">
+        ▶ Play
       </button>
     </>
   );
