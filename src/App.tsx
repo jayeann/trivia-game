96e63f358ff196321839370fdabe856a5a3984d6
jayeann <jgtagoctoc@gmail.com>
2025-05-03 21:44:41 +0800

    add store and states, import the pages based on phase

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
-import triviaLogo from "/trivia.svg";
-import playBtn from "../src/assets/play-button.svg";
-import musicBtn from "../src/assets/music-on-button.svg";
-import settingBtn from "../src/assets/setting-button.svg";
 import "./App.css";
+import { useGameStore } from "./store/gameStore";
+import Home from "./components/Home";
+import CategorySelector from "./components/CategorySelector";
+import GameScreen from "./components/GameScreen";
+import ResultScreen from "./components/ResultScreen";
 
 function App() {
-  return (
-    <>
-      <div className="flex items-center justify-center">
-        <a href="#" target="_blank">
-          <img className="size-96" src={triviaLogo} alt="trivia logo" />
-        </a>
-      </div>
-      <h1 className="uppercase text-6xl lg:text-7xl xl:text-8xl text-white font-black double-shadow">
-        Name that
-      </h1>
+  const phase = useGameStore((state) => state.phase);
 
-      <button className="fixed bottom-50 left-1/2 transform -translate-x-1/2 transition duration-300 hover:scale-120 active:scale-95 rounded-full ">
-        <img className="size-40" src={playBtn} alt="play button" />
-      </button>
-      <button className="fixed left-10 bottom-10 transform transition duration-300 hover:scale-110 active:scale-95 rounded-full ">
-        <img className="size-24" src={settingBtn} alt="settings button" />
-      </button>
-      <button className="fixed right-10 bottom-10 transform  transition duration-300 hover:scale-110 active:scale-95 rounded-full ">
-        <img className="size-24" src={musicBtn} alt="music on button" />
-      </button>
-    </>
-  );
+  switch (phase) {
+    case "HOME":
+      return <Home />;
+    case "CATEGORY":
+      return <CategorySelector />;
+    case "ROUND":
+      return <GameScreen />;
+    case "RESULTS":
+      return <ResultScreen />;
+    default:
+      return null;
+  }
 }
 
 export default App;
---
+++ src/store/gameStore.tsx
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+
+type Question = {
+  text: string;
+};
+
+type GamePhase = "HOME" | "CATEGORY" | "ROUND" | "RESULTS";
+
+type GameState = {
+  category: string | null;
+  currentRound: number;
+  questions: Question[];
+  questionsPerRound: number;
+  currentQuestionIndex: number;
+  score: number;
+  phase: GamePhase;
+
+  setPhase: (phase: GamePhase) => void;
+};
+
+export const useGameStore = create<GameState>((set) => ({
+  phase: "HOME",
+  category: null,
+  questions: [],
+  currentQuestionIndex: 0,
+  score: 0,
+  currentRound: 1,
+  questionsPerRound: 0,
+
+  setPhase: (phase) => set({ phase }),
+}));
