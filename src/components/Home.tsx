a1c5186d2033139ffca331991099d78e5f02d013
jayeann <jgtagoctoc@gmail.com>
2025-05-03 21:43:42 +0800

    add the home ui

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import triviaLogo from "/trivia.svg";
+import playBtn from "../assets/play-button.svg";
+import musicBtn from "../assets/music-on-button.svg";
+import settingBtn from "../assets/setting-button.svg";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <a href="#" target="_blank">
+          <img className="size-96" src={triviaLogo} alt="trivia logo" />
+        </a>
+      </div>
+      <h1 className="uppercase text-6xl lg:text-7xl xl:text-8xl text-white font-black double-shadow">
+        Name that
+      </h1>
+
+      <button className="fixed bottom-50 left-1/2 transform -translate-x-1/2 transition duration-300 hover:scale-120 active:scale-95 rounded-full ">
+        <img className="size-40" src={playBtn} alt="play button" />
+      </button>
+      <button className="fixed left-10 bottom-10 transform transition duration-300 hover:scale-110 active:scale-95 rounded-full ">
+        <img className="size-24" src={settingBtn} alt="settings button" />
+      </button>
+      <button className="fixed right-10 bottom-10 transform  transition duration-300 hover:scale-110 active:scale-95 rounded-full ">
+        <img className="size-24" src={musicBtn} alt="music on button" />
+      </button>
+    </>
+  );
 }
 
 export default Home;
