a0b7e2dc67179a8e40cbb65427b4fda2fb1f504d
jayeann <jgtagoctoc@gmail.com>
2025-05-03 18:33:35 +0800

    add music and settings btn and fix the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import triviaLogo from "/trivia.svg";
+import playBtn from "../src/assets/play-button.svg";
+import musicBtn from "../src/assets/music-on-button.svg";
+import settingBtn from "../src/assets/setting-button.svg";
 import "./App.css";
 
 function App() {
@@ -6,15 +9,21 @@ function App() {
     <>
       <div className="flex items-center justify-center">
         <a href="#" target="_blank">
-          <img className="size-96" src={triviaLogo} alt="Vite logo" />
+          <img className="size-96" src={triviaLogo} alt="trivia logo" />
         </a>
       </div>
-      <h1 className="uppercase text-9xl text-white font-black double-shadow">
+      <h1 className="uppercase text-6xl lg:text-7xl xl:text-8xl text-white font-black double-shadow">
         Name that
       </h1>
 
-      <button className="px-8 py-4 bg-gradient-to-br from-green-400 to-green-700 text-white text-2xl font-extrabold rounded-full shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-xl active:scale-95">
-        ▶ Play
+      <button className="fixed bottom-50 left-1/2 transform -translate-x-1/2 transition duration-300 hover:scale-120 active:scale-95 rounded-full ">
+        <img className="size-40" src={playBtn} alt="play button" />
+      </button>
+      <button className="fixed left-10 bottom-10 transform transition duration-300 hover:scale-110 active:scale-95 rounded-full ">
+        <img className="size-24" src={settingBtn} alt="settings button" />
+      </button>
+      <button className="fixed right-10 bottom-10 transform  transition duration-300 hover:scale-110 active:scale-95 rounded-full ">
+        <img className="size-24" src={musicBtn} alt="music on button" />
       </button>
     </>
   );
