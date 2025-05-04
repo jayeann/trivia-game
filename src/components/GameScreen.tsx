6d7692335fc909eb09bbb9bc5e924694be94c44f
jayeann <jgtagoctoc@gmail.com>
2025-05-04 21:25:10 +0800

    add game screen layout and add home and pause svg

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -1,19 +1,30 @@
 import "../styles/music_player.css";
+import playBtn from "../assets/play-button.svg";
+import pauseBtn from "../assets/pause-button.svg";
+import SendIcon from "../assets/SendIcons";
+
 const GameScreen = () => {
   return (
     <div className="h-screen">
-      <div className="p-15 space-y-10">
-        <h1 className="uppercase text-xl lg:text-2xl xl:text-3xl text-gray-300 font-black">
+      <div className="p-12 space-y-10">
+        <h1 className="uppercase text-2xl xl:text-3xl text-gray-300 font-black">
           Round
         </h1>
-        <h2 className="uppercase text-3xl lg:text-5xl xl:text-6xl text-white font-black">
+        <h2 className="uppercase text-5xl xl:text-6xl text-white font-black mini-double-shadow">
           Name that song
         </h2>
       </div>
       <div className="h-1/2 flex items-center justify-center">
         <div className="flex p-8 w-full min-h-1/2 space-x-10 bg-[#004357] rounded-2xl">
-          <div className="flex-1 rounded-2xl bg-white"></div>
-          <div className="flex-1 rounded-2xl ">
+          <div className="flex-1 rounded-2xl bg-white flex justify-center items-center">
+            <button className="w-20 h-20 flex items-center justify-center transform transition hover:scale-120 ">
+              <img src={playBtn} alt="play button" />
+            </button>
+            <button className="w-20 h-20 flex items-center justify-center transform transition hover:scale-120 ">
+              <img src={pauseBtn} alt="pause button" />
+            </button>
+          </div>
+          <div className="flex-1 rounded-2xl flex justify-center">
             <div className="music-player">
               <span className="bar"></span>
               <span className="bar"></span>
@@ -25,6 +36,18 @@ const GameScreen = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* Input moved to a new line below */}
+      <div className="flex justify-center p-8">
+        <input
+          type="text"
+          placeholder="Enter your answer"
+          className="w-9/10 h-20 text-3xl uppercase p-4 border rounded-l-3xl text-center bg-white"
+        />
+        <button className="w-auto text-3xl p-4 bg-amber-300 text-[#004357] rounded-r-3xl hover:bg-amber-400 transition">
+          <SendIcon className="w-10 h-10" />
+        </button>
       </div>
     </div>
   );
