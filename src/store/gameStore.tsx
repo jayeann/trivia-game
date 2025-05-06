468a0f1f5f8e92ec2909eba320e08e02121a9d92
jayeann <jgtagoctoc@gmail.com>
2025-05-06 16:51:39 +0800

    add round and question increment - update questio type

--- src/store/gameStore.tsx
+++ src/store/gameStore.tsx
@@ -2,6 +2,9 @@ import { create } from "zustand";
 
 type Question = {
   text: string;
+  mediaType: "audio" | "video" | "image" | string;
+  url?: string;
+  hint?: string;
 };
 
 type GamePhase = "HOME" | "CATEGORY" | "ROUND" | "RESULTS";
@@ -16,18 +19,31 @@ type GameState = {
   phase: GamePhase;
   loading: boolean;
 
+  setCurrentRound: (currentRound: number) => void;
+  setCurrentQuestionIndex: (currentQuestionIndex: number) => void;
   setPhase: (phase: GamePhase) => void;
+
+  incrementQuestionIndex: () => void;
+  incrementRound: () => void;
 };
 
-export const useGameStore = create<GameState>((set) => ({
+export const useGameStore = create<GameState>((set, get) => ({
   phase: "HOME",
   category: null,
   questions: [],
   currentQuestionIndex: 0,
   score: 0,
-  currentRound: 0,
+  currentRound: 1,
   questionsPerRound: 0,
   loading: false,
 
   setPhase: (phase) => set({ phase }),
+  setCurrentRound: (currentRound) => set({ currentRound }),
+  setCurrentQuestionIndex: (currentQuestionIndex) =>
+    set({ currentQuestionIndex }),
+
+  incrementQuestionIndex: () =>
+    set({ currentQuestionIndex: get().currentQuestionIndex + 1 }),
+
+  incrementRound: () => set({ currentRound: get().currentRound + 1 }),
 }));
