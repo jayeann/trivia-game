6fe33935aad08f01e9cb0e709ef2569c18b7d72c
jayeann <jgtagoctoc@gmail.com>
2025-05-06 22:16:29 +0800

    remove the next and back btn controls from the game screen

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -3,8 +3,7 @@ import SendIcon from "../assets/SendIcons";
 import MediaControls from "./MediaControls";
 import { useRounds } from "../hooks/useRounds";
 import { useGameStore } from "../store/gameStore";
-import backBtn from "../assets/back-button.svg";
-import nextBtn from "../assets/next-button.svg";
+import RoundControls from "./RoundControls";
 
 const GameScreen = () => {
   const { data: rounds, isLoading, error } = useRounds();
@@ -13,18 +12,6 @@ const GameScreen = () => {
   const currentQuestionIndex = useGameStore(
     (state) => state.currentQuestionIndex
   );
-
-  const setPhase = useGameStore((state) => state.setPhase);
-
-  const setCurrentQuestionIndex = useGameStore(
-    (state) => state.setCurrentQuestionIndex
-  );
-
-  const incrementQuestionIndex = useGameStore(
-    (state) => state.incrementQuestionIndex
-  );
-
-  const incrementRound = useGameStore((state) => state.incrementRound);
 
   //show the specific ui based on state
   if (isLoading) return <p>Loading rounds...</p>;
@@ -35,49 +22,28 @@ const GameScreen = () => {
   const currentQuestion = roundContent.questions[currentQuestionIndex];
   const isMediaAudio = currentQuestion?.mediaType === "audio" ? true : false;
   const isImage = currentQuestion?.mediaType === "image";
-  const isLastQuestion =
-    currentQuestionIndex === roundContent.questions.length - 1;
-  const isLastRound = currentRound === rounds.length;
-  const handleNext = () => {
-    if (!isLastQuestion) {
-      incrementQuestionIndex();
-    } else if (!isLastRound) {
-      incrementRound();
-      setCurrentQuestionIndex(0); // reset for next round
-    } else {
-      setPhase("RESULTS");
-    }
-  };
 
   return (
-    <div className="h-screen">
-      <>
-        <button className="fixed left-10 top-10 transform transition duration-300 hover:scale-110 active:scale-95 rounded-full ">
-          <img className="size-24" src={backBtn} alt="settings button" />
-        </button>
-        <button
-          className="fixed right-10 top-10 transform  transition duration-300 hover:scale-110 active:scale-95 rounded-full "
-          onClick={handleNext}
-        >
-          <img className="size-24" src={nextBtn} alt="music on button" />
-        </button>
-      </>
-      <div className="p-12 space-y-10">
+    <div className="h-screen w-full">
+      <RoundControls />
+      <div className="py-12 space-y-10">
         <h1 className="uppercase text-2xl xl:text-3xl text-gray-300 font-black">
-          Round {roundContent.roundNumber}
+          Round {roundContent.roundNumber}: {currentQuestionIndex + 1}/
+          {roundContent.questions.length}
         </h1>
         <h2 className="uppercase text-5xl xl:text-6xl text-white font-black mini-double-shadow">
-          {roundContent.title} {currentQuestionIndex + 1}/
-          {roundContent.questions.length}
+          {roundContent.title}
         </h2>
       </div>
 
       {isImage ? (
-        <img
-          className="w-full h-1/3"
-          src={currentQuestion?.url}
-          alt={currentQuestion?.answer}
-        />
+        <div className="h-1/2  flex items-center justify-center">
+          <img
+            className="w-auto h-1/2"
+            src={currentQuestion?.url}
+            alt={currentQuestion?.answer}
+          />
+        </div>
       ) : (
         <MediaControls isAudio={isMediaAudio} url={currentQuestion?.url} />
       )}
@@ -86,7 +52,7 @@ const GameScreen = () => {
         <input
           type="text"
           placeholder="Enter your answer"
-          className="w-9/10 h-20 text-3xl uppercase p-4 border rounded-l-3xl text-center bg-white"
+          className="w-full h-20 text-3xl uppercase p-4 border rounded-l-3xl text-center bg-white"
         />
         <button className="w-auto text-3xl p-4 bg-amber-300 text-[#004357] rounded-r-3xl hover:bg-amber-400 transition">
           <SendIcon className="w-10 h-10" />
