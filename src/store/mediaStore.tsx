0c7047d6cdbe62cc852ec549c7ca96f9cb316f45
jayeann <jgtagoctoc@gmail.com>
2025-05-07 12:39:27 +0800

    add state to handle background music

--- src/store/mediaStore.tsx
+++ src/store/mediaStore.tsx
@@ -2,12 +2,16 @@ import { create } from "zustand";
 
 type MediaState = {
   isPlaying: boolean;
+  isBgPlaying: boolean;
 
   setIsPlaying: (isPlaying: boolean) => void;
+  setIsBgPlaying: (isBgPlaying: boolean) => void;
 };
 
 export const useMediaStore = create<MediaState>((set) => ({
   isPlaying: true,
+  isBgPlaying: true,
 
   setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
+  setIsBgPlaying: (isBgPlaying: boolean) => set({ isBgPlaying }),
 }));
