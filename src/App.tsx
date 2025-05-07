a6ec78fea9072c36585b4e8facedcdc5ce52a7aa
jayeann <jgtagoctoc@gmail.com>
2025-05-07 12:31:56 +0800

    add bg audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { useEffect } from "react";
 import "./App.css";
 import { useGameStore } from "./store/gameStore";
+import { useMediaStore } from "./store/mediaStore";
 import Home from "./components/Home";
 import CategorySelector from "./components/CategorySelector";
 import GameScreen from "./components/GameScreen";
 import ResultScreen from "./components/ResultScreen";
-import Footer from "./components/Footer";
+import { Audio } from "./components/MediaPlayer";
 
 function App() {
   const phase = useGameStore((state) => state.phase);
+  const setIsBgPlaying = useMediaStore((state) => state.setIsBgPlaying);
+  const isBgPlaying = useMediaStore((state) => state.isBgPlaying);
+
+  useEffect(() => {
+    // Ensure background music plays on mount
+    setIsBgPlaying(true);
+  }, [setIsBgPlaying]);
+
   let content;
   switch (phase) {
     case "HOME":
@@ -28,8 +38,13 @@ function App() {
 
   return (
     <>
+      <Audio
+        onEnded={() => {}}
+        volume={0.15}
+        playing={isBgPlaying}
+        url="https://raw.githubusercontent.com/jayeann/media-player-test/refs/heads/main/song/bg.mp3"
+      />
       {content}
-      <Footer />
     </>
   );
 }
