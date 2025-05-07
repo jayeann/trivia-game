5ab9dc403c1c16761daf6d6b71806084bcc4b1ca
jayeann <jgtagoctoc@gmail.com>
2025-05-07 14:30:43 +0800

    add mock data for categories

---
+++ src/data/sampleQuestions.ts
@@ -0,0 +1,102 @@
+export const mockData = [
+  {
+    category: "1980's",
+    rounds: [
+      [
+        {
+          question: "Music 1 Name this song...",
+          answer: "A",
+          media_type: "mp3",
+          media_url: "",
+        },
+        {
+          question: "Music 1 Name this song...",
+          answer: "B",
+          media_type: "mp4",
+          media_url: "",
+        },
+      ],
+      [
+        {
+          question: "Music 2 Name this song...",
+          answer: "C",
+          media_type: "mp3",
+          media_url: "",
+        },
+        {
+          question: "Music 2 Name this song...",
+          answer: "D",
+          media_type: "mp4",
+          media_url: "",
+        },
+      ],
+    ],
+  },
+  {
+    category: "1990's",
+    rounds: [
+      [
+        {
+          question: "Art Name this song...",
+          answer: "C",
+          media_type: "mp3",
+          media_url:
+            "https://raw.githubusercontent.com/jayeann/media-player-test/refs/heads/main/show/show-1.png",
+        },
+        {
+          question: "Art Name this song...",
+          answer: "D",
+          media_type: "mp4",
+          media_url: "",
+        },
+      ],
+      [
+        {
+          question: "2 Art Name this song...",
+          answer: "E",
+          media_type: "mp3",
+          media_url: "",
+        },
+        {
+          question: "2 Art Name this song...",
+          answer: "F",
+          media_type: "mp4",
+          media_url: "",
+        },
+      ],
+    ],
+  },
+  {
+    category: "2000's",
+    rounds: [
+      [
+        {
+          question: "Sport Name this song...",
+          answer: "w",
+          media_type: "mp3",
+          media_url: "",
+        },
+        {
+          question: "Sport Name this song...",
+          answer: "x",
+          media_type: "mp4",
+          media_url: "",
+        },
+      ],
+      [
+        {
+          question: "2 Sport Name this song...",
+          answer: "Y",
+          media_type: "mp3",
+          media_url: "",
+        },
+        {
+          question: "2 Sport Name this song...",
+          answer: "Z",
+          media_type: "mp4",
+          media_url: "",
+        },
+      ],
+    ],
+  },
+];
